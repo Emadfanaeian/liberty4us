5a7b81808099c1868d4dfe4c0d921029745483aa
emad fanaeian <emadfanaeian@gmail.com>
2022-11-23 14:33:14 +0330

    init

---
+++ articles/models.py
@@ -0,0 +1,14 @@
+"""Article Model"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Article(models.Model):
+    """This Is Articles Model"""
+    title = models.CharField(max_length=255)
+    short_information = models.TextField()
+    is_published = models.BooleanField(default=False)
+    total_approved = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
